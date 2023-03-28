bb69f8291a8fc9636de14152c3d9c616d3234dd3
joaogilramos <joaogilramos2003@gmail.com>
2023-03-28 17:42:19 +0100

    Primeiras altrações

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	
+}
+
+function draw() {
+	background(255);
+	noFill();
+	stroke(50,255);
+	circle(width /2, height /2, height / 3,);
+	circle(width /2 - height /3/4, height /2 - height/18, height /12);
+	circle(width /2 - height /3/4, height /2 - height/18, height /40);
+	circle(width /2 + height /3/4, height /2 - height/18, height /12);
+	circle(width /2 + height /3/4, height /2 - height/18, height /40);
+	circle(width /2, height /2, height / 40,);
+
+
+	
+
+}

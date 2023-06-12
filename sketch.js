af29b66e9bfb4c810a1d7cf92eb39ed5b73c5e6d
joaogilramos <joaogilramos2003@gmail.com>
2023-06-12 12:52:40 +0100

    código

---
+++ sketch.js
@@ -0,0 +1,30 @@
+
+  function setup() {
+    createCanvas(400, 400);
+  }
+  
+  
+  function draw() {
+    background(255);
+  
+  
+    fill(255, 204, 0);
+    ellipse(200, 200, 150, 200);
+  
+    fill(255);
+    ellipse(175, 170, 40, 50);
+    ellipse(225, 170, 40, 50);
+  
+    fill(0);
+    ellipse(175, 170, 20, 30);
+    ellipse(225, 170, 20, 30);
+  
+  
+    fill(255);
+    rect(170, 230, 60, 20);
+    fill(255, 0, 0);
+    rect(170, 230, 60, 10);
+  }
+  
+  
+  new p5();
